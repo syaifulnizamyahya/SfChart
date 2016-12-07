401eef1483cf7cd068d7cf3c1c2789bfacd6955c
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2016-12-07 17:24:57 +0800

    Initial commit

---
+++ SfChart/SfChart/Models/GoldDemand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfChart.Models
+{
+	public class GoldDemand
+	{
+		public string Demand { get; set; }
+		public double Year2010 { get; set; }
+		public double Year2011 { get; set; }
+	}
+}
---
+++ SfChart/SfChart/ViewModels/ChartViewModel.cs
@@ -0,0 +1,43 @@
+﻿using SfChart.Models;
+using System.Collections.ObjectModel;
+using System;
+
+namespace SfChart.ViewModels
+{
+	public class ChartViewModel
+	{
+		public ObservableCollection<GoldDemand> Demands { get; set; } = new ObservableCollection<GoldDemand>();
+
+		public void InitDemands()
+		{
+			this.Demands = new ObservableCollection<GoldDemand>
+			{
+				new GoldDemand()
+				{
+					Demand = "Jewelry", Year2010 = 1998.0, Year2011 = 2361.2
+				},
+				new GoldDemand()
+				{
+					Demand = "Electronics", Year2010 = 1284.0, Year2011 = 1328.0
+				},
+				new GoldDemand()
+				{
+					Demand = "Research", Year2010 = 1090.5, Year2011 = 1032.0
+				},
+				new GoldDemand()
+				{
+					Demand = "Investment", Year2010 = 1643.0, Year2011 = 1898.0
+				},
+				new GoldDemand()
+				{
+					Demand = "Bank Purchases", Year2010 = 987.0, Year2011 = 887.0
+				}
+			};
+		}
+
+		public void InitSampleData()
+		{
+			InitDemands();
+		}
+	}
+}
