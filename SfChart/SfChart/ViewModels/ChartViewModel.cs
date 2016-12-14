1815d249f23e02d00634113d1410278d2c51eaa7
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2016-12-14 11:52:53 +0800

    Cleaned up code

--- SfChart/SfChart/ViewModels/ChartViewModel.cs
+++ SfChart/SfChart/ViewModels/ChartViewModel.cs
@@ -2,57 +2,78 @@
 using System.Collections.ObjectModel;
 using System;
 using Syncfusion.UI.Xaml.Charts;
+using System.Diagnostics;
 
 namespace SfChart.ViewModels
 {
 	public class ChartViewModel
 	{
-		public ObservableCollection<Curve> ChartSeries { get; set; } = new ObservableCollection<Curve>();
+		public ObservableCollection<Curve> RssiSeries { get; set; } = new ObservableCollection<Curve>();
 
+		public ChartSeriesCollection RssiCollection { get; set; } = new ChartSeriesCollection();
 
-		public void InitCurve()
+		public void InitSampleData()
+		{
+			InitRssi();
+			InitRssiCollection();
+		}
+
+		private void InitRssi()
 		{
 			Random random = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
 			int min = 20;
 			int max = 90;
 
-			for(int j = 0; j<5; j++)
+			for (int i = 0; i < 5; i++)
 			{
+				var rssiPoint = new Point();
+				rssiPoint.x = random.Next(-2, 13);
+				rssiPoint.y = random.Next(min, max);
+
+				var aPoint = new Point();
+				aPoint.x = rssiPoint.x - 2;
+				aPoint.y = 0;
+
+				var bPoint = new Point();
+				bPoint.x = rssiPoint.x - 1;
+				bPoint.y = rssiPoint.y - (rssiPoint.y / 5);
+
+				var dPoint = new Point();
+				dPoint.x = rssiPoint.x + 1;
+				dPoint.y = rssiPoint.y - (rssiPoint.y / 5);
+
+				var ePoint = new Point();
+				ePoint.x = rssiPoint.x + 2;
+				ePoint.y = 0;
+
 				var curve = new Curve();
+				curve.Points.Add(aPoint);
+				curve.Points.Add(bPoint);
+				curve.Points.Add(rssiPoint);
+				curve.Points.Add(dPoint);
+				curve.Points.Add(ePoint);
 
-				for (int i = 0; i < 5; i++)
-				{
-					var point = new Point();
-					point.x = random.Next(min, max);
-					point.y = random.Next(min, max);
+				RssiSeries.Add(curve);
 
-					curve.Points.Add(point);
-				}
-
-				ChartSeries.Add(curve);
+				Debug.WriteLine(RssiSeries.Count + " A " + curve.Points[0].x + " " + curve.Points[0].y);
+				Debug.WriteLine(RssiSeries.Count + " B " + curve.Points[1].x + " " + curve.Points[1].y);
+				Debug.WriteLine(RssiSeries.Count + " R " + curve.Points[2].x + " " + curve.Points[2].y);
+				Debug.WriteLine(RssiSeries.Count + " D " + curve.Points[3].x + " " + curve.Points[3].y);
+				Debug.WriteLine(RssiSeries.Count + " E " + curve.Points[4].x + " " + curve.Points[4].y);
 			}
 		}
-
-		public void InitSampleData()
-		{
-			InitCurve();
-			InitSeriesCollection();
-		}
-
-		public ChartSeriesCollection SeriesCollection { get; set; } = new ChartSeriesCollection();
-
-		private void InitSeriesCollection()
+		private void InitRssiCollection()
 		{
 			Random random = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
 
-			for (int i = 0; i< ChartSeries.Count; i++)
+			for (int i = 0; i < RssiSeries.Count; i++)
 			{
 				var splineSeries = new SplineSeries();
-				splineSeries.ItemsSource = ChartSeries[i].Points;
+				splineSeries.ItemsSource = RssiSeries[i].Points;
 				splineSeries.XBindingPath = "x";
 				splineSeries.YBindingPath = "y";
 				splineSeries.Label = random.Next().ToString();
-				SeriesCollection.Add(splineSeries);
+				RssiCollection.Add(splineSeries);
 			}
 		}
 	}
